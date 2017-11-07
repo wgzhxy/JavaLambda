485111acb64900c91132f9b48c5a04f0f64af807
东往 <wgzhxy@163.com>
2017-11-07 12:09:17 +0800

    update PredicateTest

--- collections/src/main/java/org/wanggz/PredicateTest.java
+++ collections/src/main/java/org/wanggz/PredicateTest.java
@@ -1,4 +1,40 @@
 package org.wanggz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.function.Predicate;
+
 public class PredicateTest {
+
+    @Test
+    public void testPredicate() {
+        Predicate<String> predicate = (str) -> str.contains("test");
+        Assert.assertTrue(predicate.test("test001"));
+    }
+
+    @Test
+    public void testPredicate_negate() {
+        Predicate<String> predicate = (str) -> str.contains("test");
+        Assert.assertTrue(predicate.negate().test("abcd"));
+    }
+
+    @Test
+    public void testPredicate_or() {
+        Predicate<String> predicate = (str) -> str.contains("test");
+        Assert.assertTrue(predicate.or(x -> x.contains("abcd")).test("test"));
+        Assert.assertTrue(predicate.or(x -> x.contains("12345")).test("12345"));
+    }
+
+    @Test
+    public void testPredicate_and() {
+        Predicate<String> predicate = (str) -> str.contains("test");
+        Assert.assertTrue(predicate.and(x -> x.contains("abcd")).test("test and abcd, 哈哈"));
+    }
+
+    @Test
+    public void testPredicate_equals() {
+        Predicate<String> predicate = (str) -> str.contains("test");
+        Assert.assertTrue(Predicate.isEqual(predicate).test(predicate));
+    }
 }
