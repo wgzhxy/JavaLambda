8ed7f174673e3987d13b4199b3bcfa0f46d3c214
东往 <wgzhxy@163.com>
2017-11-08 10:15:25 +0800

    flatMap case

--- collections/src/main/java/org/wanggz/ListTest.java
+++ collections/src/main/java/org/wanggz/ListTest.java
@@ -1,16 +1,36 @@
 package org.wanggz;
 
-import com.google.common.collect.Lists;
+import com.alibaba.fastjson.JSON;
 import org.junit.Test;
+import org.wanggz.dto.Student;
+import org.wanggz.dto.StudentNew;
 import org.wanggz.utils.StudentTools;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ListTest {
 
     @Test
-    public void testList() {
+    public void testList_flatMap() {
 
-        StudentTools.getStudents().stream().limit(10).flatMap(x -> Lists.newArrayList(x).stream());
+        List<StudentNew> tmpLs = StudentTools.getStudents().stream()
+                .limit(3)
+                .flatMap(student -> {
+                    StudentNew studentNew = new StudentNew();
+                    studentNew.setDesc(student.getName() + "->" + student.getAge() + "->" + student.getSex());
+                    studentNew.setName(student.getName());
+                    studentNew.setAge(student.getAge());
+                    studentNew.setSex(student.getSex());
+                    return Stream.of(studentNew);
+                }).collect(Collectors.toList());
 
+        System.out.println(JSON.toJSONString(tmpLs));
+    }
 
+    @Test
+    public void test_Stream_of() {
+        System.out.println(Stream.of("1", " 2", "3").collect(Collectors.toList()));
     }
 }
